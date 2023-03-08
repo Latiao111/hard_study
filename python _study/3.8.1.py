bdd0bbe06581d6e54e6658deabc009537b0ad2c0
Latiao111 <you@example.com>
2023-03-08 21:30:51 +0800

    love

---
+++ python _study/3.8.1.py
@@ -0,0 +1,41 @@
+
+# names=['Bob','Tom','Alice','Jerry','Wendy','Smith']
+# new_names=[name.upper()for name in names if len(name)>3]
+# print(new_names)
+
+# multiples=[i for i in range(30)if i%3==0]
+# print(multiples)
+
+# listdemo=['Google','runble','Taobao']
+# newdict={key:len(key)for key in listdemo}
+# print(newdict)
+
+# a=(x for x in range(1,10))
+
+# name='runoob'
+# print(f'Hello {name}')
+
+# str='abc'
+# capitalize(str)
+# print(str)
+
+# list=['red' ,'green','black','white','black']
+# list.append('brown')
+# print(list)
+# print(list[0])
+# print(list[2])
+
+
+# tinydict={'book':'python','numb':8,'love':'you'}
+# print(tinydict)
+# tinydict['numb']=9
+# print(tinydict)
+# tinydict['book']='pytorch'
+# print(tinydict)
+# tinydict['lick']='you'
+# print(tinydict)
+# print(tinydict['numb'])
+# del tinydict['book']
+# print(tinydict)
+
+
